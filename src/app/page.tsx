b27c7332d5eb8ac2b81570385001c8254f846ae8
Tayse Rosa <tayse_rosa@yahoo.com.br>
2025-05-23 10:54:31 -0300

    Emoji Ratings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
-import { Square, Circle } from "@/components/Formas";
-import { Person } from "@/components/Person";
-import { Card } from "@/components/Card";
-import { peopleList } from "@/data/peopleList";
+import { EmojiRating } from "@/components/EmojiRating";
 
 export default function Page() {
-  const fullTime = new Intl.DateTimeFormat("pt-Br", {
-    timeStyle: "short",
-    hour12: false,
-  }).format();
-
-  const hour = new Date().getHours();
-  let greeting = "";
-  if (hour >= 0 && hour < 12) {
-    greeting = "Bom dia ☀️";
-  } else if (hour >= 12 && hour < 18) {
-    greeting = "Boa tarde ☀️";
-  } else {
-    greeting = "Boa noite 🌕";
-  }
-
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl bold">{greeting}</div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center ">
+      <EmojiRating rate={3.2} />
     </div>
   );
 }
