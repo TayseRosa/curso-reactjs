0897a33aff620cd0242d645de2d3fb59771f9589
Tayse Rosa <tayse_rosa@yahoo.com.br>
2025-05-23 09:31:25 -0300

    Javascript - Filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,31 @@ import { Card } from "@/components/Card";
 import { peopleList } from "@/data/peopleList";
 
 export default function Page() {
+  const chemists = peopleList.filter(
+    (person) => person.profession === "chemist"
+  );
+
   return (
     <div>
+      {chemists.length > 0 && (
+        <>
+          <h3>Lista de químicos</h3>
+          <ul>
+            {chemists.map((person) => (
+              <li>
+                {person.name} - {person.profession}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+
+      <hr />
+
       <ul>
         {peopleList.map((person) => (
           <li key={person.id}>
-            {" "}
-            {person.id} - {person.name}
+            {person.id} - {person.name} - {person.profession}
           </li>
         ))}
       </ul>
